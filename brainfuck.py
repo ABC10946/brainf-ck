4c28469d0cee5f3739a964821b896e36a330b55e
ABC10946 <alphalex10946@gmail.com>
2017-05-02 08:52:12 +0900

    update for python3

--- brainfuck.py
+++ brainfuck.py
@@ -25,14 +25,14 @@ def bf_interpreter(script,mode=0):
         elif _script[sc] == ">":
             p += 1
             if p > 31:
-                print "error out of memory!"
+                print("error out of memory!")
                 error_flag = 1
                 break
 
         elif _script[sc] == "<":
             p -= 1
             if p < 0:
-                print "error out of memory!"
+                print("error out of memory!")
                 error_flag = 1
                 break
 
@@ -49,7 +49,7 @@ def bf_interpreter(script,mode=0):
 
         elif _script[sc] == ".":
             if mode == 1 or mode == 2:
-                print "output:",chr(memory[p])
+                print("output:",chr(memory[p]))
 
 
             output.append(chr(memory[p]))
@@ -58,14 +58,14 @@ def bf_interpreter(script,mode=0):
             try:
                 memory[p] = input("input:")
             except EOFError:
-                print ""
+                print("")
                 break
             except KeyboardInterrupt:
-                print ""
+                print("")
                 break
 
         else:
-            print "error \""+script[sc]+"\" command is undefined."
+            print("error \""+script[sc]+"\" command is undefined.")
             error_flag = 1
             break
 
@@ -76,38 +76,38 @@ def bf_interpreter(script,mode=0):
         elif mode == 2: #output immediately
             pass
         elif mode == 1: #step mode
-            print "counter:",count
-            print "memory"
-            print memory
+            print("counter:",count)
+            print("memory")
+            print(memory)
             if p == 0: #memory cursor
-                print " ^"
+                print(" ^")
             elif p == memory[len(memory)-1]:
                 s_memory = map(str,memory)
-                print " "*(len(", ".join(s_memory))-1)+"^"
+                print(" "*(len(", ".join(s_memory))-1)+"^")
             else:
                 s_memory = map(str,memory[0:p+1])
-                print " "*(len(", ".join(s_memory)))+"^"
+                print(" "*(len(", ".join(s_memory)))+"^")
 
             #script cursor
-            print "script"
-            print script
-            print " "*(sc-1)+"^"
+            print("script")
+            print(script)
+            print(" "*(sc-1)+"^")
             try:
                 raw_input()
             except EOFError:
-                print ""
+                print("")
                 break
             except KeyboardInterrupt:
-                print ""
+                print("")
                 break
 
         else:
-            print "error mode is undefined."
+            print("error mode is undefined.")
             error_flag = 1
             break
 
     if error_flag == 0:
-        print "".join(output)
+        print("".join(output))
 
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
@@ -123,6 +123,6 @@ if __name__ == "__main__":
                 bf_interpreter(script)
 
         except IOError:
-            print "file not found!"
+            print("file not found!")
     else:
-        print "error file undefined!"
+        print("error file undefined!")
